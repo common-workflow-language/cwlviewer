71b31729686a1887abc4ac7cdde1907635484a7f
Mark Robinson <mark.robinson-4@student.manchester.ac.uk>
2017-03-10 14:13:28 +0000

    Allow the use of the dot character in workflow IDs
    Closes #90

---
+++ src/main/java/org/commonwl/viewer/MongoConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.commonwl.viewer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoConfig {
+
+    @Autowired
+    private MongoDbFactory mongoFactory;
+
+    @Autowired
+    private MongoMappingContext mongoMappingContext;
+
+    /**
+     * Override the default converter which just throws an error with special chars
+     */
+    @Bean
+    public MappingMongoConverter mongoConverter() throws Exception {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoFactory);
+        MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+
+        // Replace dot with the unicode replacement character
+        mongoConverter.setMapKeyDotReplacement("\uFFFD");
+
+        return mongoConverter;
+    }
+}
