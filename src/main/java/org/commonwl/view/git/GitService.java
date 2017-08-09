2e0e60fd0126d326bf661c49f674ff6ba22d3c78
Mark Robinson <mark@goldtek.co.uk>
2017-08-09 12:32:28 +0100

    Remove debug listing of branches

--- src/main/java/org/commonwl/view/git/GitService.java
+++ src/main/java/org/commonwl/view/git/GitService.java
@@ -22,7 +22,6 @@ package org.commonwl.view.git;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.commonwl.view.researchobject.HashableAgent;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.PersonIdent;
@@ -40,7 +39,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.apache.jena.ext.com.google.common.io.Files.createTempDir;
@@ -110,18 +108,6 @@ public class GitService {
 
             // Checkout the specific branch or commit ID
             if (repo != null) {
-                System.out.println("Listing local branches:");
-                    List<Ref> call = repo.branchList().call();
-                    for (Ref ref : call) {
-                        System.out.println("Branch: " + ref + " " + ref.getName() + " " + ref.getObjectId().getName());
-                    }
-
-                    System.out.println("Now including remote branches:");
-                    call = repo.branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
-                    for (Ref ref : call) {
-                        System.out.println("Branch: " + ref + " " + ref.getName() + " " + ref.getObjectId().getName());
-                    }
-
                 // Create a new local branch if it does not exist and not a commit ID
                 boolean createBranch = !ObjectId.isId(gitDetails.getBranch());
                 if (createBranch) {
