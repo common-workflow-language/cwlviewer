439971e774a2c8f2fc89a69f57eb6c918575f531
Stian Soiland-Reyes <stain@apache.org>
2017-09-15 17:08:31 +0100

    Added MultipleWorkflowsException
    .. which shows representations as text/uri-list
    and permalinks per format

--- src/main/java/org/commonwl/view/GlobalControllerErrorHandling.java
+++ src/main/java/org/commonwl/view/GlobalControllerErrorHandling.java
@@ -19,6 +19,9 @@
 
 package org.commonwl.view;
 
+import java.util.Collections;
+
+import org.commonwl.view.workflow.MultipleWorkflowsException;
 import org.commonwl.view.workflow.RepresentationNotFoundException;
 import org.commonwl.view.workflow.WorkflowNotFoundException;
 import org.springframework.http.HttpHeaders;
@@ -29,7 +32,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
- * Handles exception handling across the application
+ * Handles exception handling across the application.
+ * <p>
+ * Because of Spring Boot's content negotiation these handlers are needed when
+ * the error is returned with an otherwise "non acceptable" content type (e.g.
+ * Accept: image/svg+xml but we have to say 404 Not Found)
+ *
  */
 @ControllerAdvice
 public class GlobalControllerErrorHandling {
@@ -42,20 +50,34 @@ public class GlobalControllerErrorHandling {
     public ResponseEntity<?> handleNotFound() {
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.TEXT_PLAIN);
-        return new ResponseEntity<>("Workflow could not be found", headers, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("Workflow or git commit could not be found", headers, HttpStatus.NOT_FOUND);
     }
 
     /**
-     * Workflow exists but representation is not found
-     * eg Generic git workflow asking for raw workflow URL
+     * More than one workflow (or workflow parts) found
+     *
+     * @return A text/uri-list of potential representations
+     */
+    @ExceptionHandler(MultipleWorkflowsException.class)
+    public ResponseEntity<?> handleMultipleWorkflows(MultipleWorkflowsException ex) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("text/uri-list"));
+        return new ResponseEntity<>(ex.toString(), headers, HttpStatus.MULTIPLE_CHOICES);
+    }
+
+    /**
+     * Workflow exists but representation is not found eg Generic git workflow
+     * asking for raw workflow URL
+     *
      * @return A plain text error message
      */
     @ExceptionHandler(RepresentationNotFoundException.class)
     public ResponseEntity<?> handleNoRepresentation() {
         final HttpHeaders headers = new HttpHeaders();
+        headers.setVary(Collections.singletonList("Accept"));
         headers.setContentType(MediaType.TEXT_PLAIN);
         return new ResponseEntity<>("The workflow exists but the requested representation could not be found",
-                headers, HttpStatus.NOT_FOUND);
+                headers, HttpStatus.NOT_ACCEPTABLE);
     }
 
 }
